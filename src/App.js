2a3b5c15a5f9375247a118ad0c9459f3695b75d8
hitesh_nathani <hitesh.nathani@groovyweb.co>
2024-11-30 14:36:37 +0530

    "Add app js file and css file"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Video from "./Video/Video";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Tiktok
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Lets build a Full Stack MERN Tik tok clone</h1>
+      <div className="app__videos">
+        <Video />
+        <Video />
+      </div>
+      {/* app container */}
+
+      {/* videos */}
     </div>
   );
 }
